6cd1f0994310e73464054ea2229d4cf72ef26f92
bitanu <bitanu.ch@gmail.com>
2020-05-29 23:56:46 +0530

    Added viewsets and routers

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path, include
-from .views import PostList, PostDetail
+#from django.urls import path, include
+#from .views import PostList, PostDetail, UserList, UserDetail
 
-urlpatterns = [
-    path('', PostList.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-]
+#urlpatterns = [
+#    path('', PostList.as_view()),
+#    path('<int:pk>/', PostDetail.as_view()),
+#    path('users/', UserList.as_view()),
+#   path('users/<int:pk>/', UserDetail.as_view()),
+#]
+
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from .views import UserViewSet, PostViewSet
+
+router = SimpleRouter()
+router.register('users', UserViewSet, base_name='users')
+router.register('', PostViewSet, base_name='posts')
+
+urlpatterns = router.urls
